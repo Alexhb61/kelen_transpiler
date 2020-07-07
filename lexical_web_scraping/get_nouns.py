4a4e37ada3d9d188eaaf8b5ce4d4f3cc97f46c91
Alexhb61 <abistagn@ucsc.edu>
2020-07-07 02:30:51 -0700

    Web scaping mostly works, but I need to not DDOS the server, I need a sleep(1)

--- lexical_web_scraping/get_nouns.py
+++ lexical_web_scraping/get_nouns.py
@@ -1,4 +1,4 @@
-from requests import post
+from requests import post, get
 from bs4 import BeautifulSoup, UnicodeDammit
 import re
 import chardet
@@ -7,39 +7,28 @@ import chardet
 
 #CODE FAILURES
 # err_str = r"āēīōūñŋþλ"
-# def swap_encoding(target_str) :
-	# return re.sub(r"ā",b'\xC4\x81'.decode('utf-8'),
-	# # re.sub(r"ē",chr(275),
-	# # re.sub(r"ī",chr(299),
-	# # re.sub(r"ō",chr(333),
-	# # re.sub(r"ū",chr(363),
-	# # re.sub(r"ñ",chr(241),
-	# # re.sub(r"ŋ",chr(0x14B),
-	# # re.sub(r"þ",chr(354),
-	# re.sub(r"λ",chr(955), target_str))
-	# #)))))))
-#print('\N{Latin Small Letter A with macron}')
-# print(swap_encoding(err_str))
-# print(err_str)
-#non_bmp_map = {
-#	r"ā" : "aa",
-#	r"ē" : "ee",
-#	r"ī" : "ii"
-#}
-#print();
-# print(r"āēīōūñŋþλ".translate(non_bmp_map))
 # https://www.dataquest.io/blog/web-scraping-beautifulsoup/ lesson
-
+output = open("kelen words.csv",'x', encoding ='utf-16-le')
 url = "https://www.terjemar.net/kelen/Dict/dictlisting.php"
 urlStem = "https://www.terjemar.net/kelen/Dict/"
 response = post(url, data={'passed': '', 'QueryType': "Form" })
-safe_text = response.text.translate(non_bmp_map)
+safe_text = response.text#.translate(non_bmp_map)
 html_soup = BeautifulSoup(safe_text, 'html.parser')
 word_links = html_soup.find_all('a', href =re.compile('formlisting'))
 is_word = re.compile('-')
 
 for link in word_links :
-	form = link.find("span", class_ = "kelen field").get_text();
+	form = link.find("span", class_ = "kelen field").get_text()
 	if not is_word.search(form) :
 		url = urlStem + link.get('href')
-		print(url)
+		small_response = get(url)
+		small_soup = BeautifulSoup(small_response.text,'html.parser')
+		small_soup.h2.a.decompose()
+		part_of_speech = small_soup.h2.get_text()
+		print(part_of_speech)
+		kelen = small_soup.ul.find_all("span",class_ = "kelen")
+		stem = ''
+		for txt in kelen :
+			stem = txt.get_text()
+		output.write(form + ',' + part_of_speech + ',' + stem + '\n')
+output.close()
